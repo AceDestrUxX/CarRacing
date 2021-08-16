5225ea472cbac7619ac8862fe3b0634bf30cbf4a
AceDestrUxX <76511704+AceDestrUxX@users.noreply.github.com>
2021-08-16 19:53:13 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var gameState = 0;
+var playerCount = 0;
+var player, game, form;
+var database;
+var canvas;
+var bgImg;
+var allPlayers;
+
+function setup() {
+  canvas = createCanvas(400,400);
+  database = firebase.database();
+  game = new Game()
+  game.getState();
+  game.start();
+}
+
+function draw() {
+  if(playerCount === 4){
+    game.update(1)
+  }
+  if(gameState === 1){
+    clear();
+    game.play()
+  }
+}
